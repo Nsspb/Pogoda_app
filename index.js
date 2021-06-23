4e1d972e95699a90c13700c29b5669411c45e7a9
Konstantin <konstantin@example.com>
2021-06-23 16:19:59 +0300

    sozdanie_stiley_css

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 let express = require('express')
+
+let app = express()
+app.set('view engine', 'ejs')
+app.use(express.static ('public'))
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.params)
+    res.render('index')
+})
+
+app.listen(1488, () =>{ 
+    console.log ('Сервер запущен на порту  - 1488')
+})
