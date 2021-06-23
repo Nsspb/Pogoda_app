2c4041169354aa64f1dee8cbee8b994deceab8dd
Konstantin <konstantin@example.com>
2021-06-24 01:59:45 +0300

    finish

--- index.js
+++ index.js
@@ -6,21 +6,22 @@ let app = express()
 // e644ee2b4c2b5a3d2e1226ccdd4a3cd0
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { weather: null, error: null })
 })
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     const { city } = req.body
 
 
-    weatherRequest(city)
-    res.render('index')
+    const { weather, error } = await weatherRequest(city)
+
+    res.render('index', { weather, error })
 })
 
-app.listen(1488, () =>{ 
-    console.log ('Сервер запущен на порту  - 1488')
+app.listen(1488, () => {
+    console.log('Сервер запущен на порту  - 1488')
 })
--- requests/weather.requests.js
+++ requests/weather.requests.js
@@ -5,5 +5,41 @@ module.exports =  async function (city){
     if (!city) {
         throw new Error ('Нет города')
     }
-console.log('city:', city)
+
+    const KEY = 'e644ee2b4c2b5a3d2e1226ccdd4a3cd0'
+    const uri = 'http://api.openweathermap.org/data/2.5/weather'
+    // const uri = 'http://api.openweathermap.org/data/2.5/weather'
+    const options = {
+        uri,
+        qs: {
+            appid: KEY,
+            q: city,
+            units: 'imperial'
+        },
+       json: true
+    }
+
+try {
+    const data = await rp(options)
+    const celsius = (data.main.temp - 32) * 5/9
+
+
+    return {
+        weather: `${data.name}: ${celsius.toFixed(0)}`,
+        error: null
+    }
+
+} catch (error) {
+    // console.log(error)
+    return {
+
+        weather: null,
+        error: error.error.message
+    }
+
+}
+
+
+
+   
 } 
